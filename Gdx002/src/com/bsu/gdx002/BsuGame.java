dc6538b6a183f4fde18465aee118b2c23ea45982
fengchong <fengchong@126.com>
2013-05-05 21:34:21 +0800

    增加3个logo界面

--- Gdx002/src/com/bsu/gdx002/BsuGame.java
+++ Gdx002/src/com/bsu/gdx002/BsuGame.java
@@ -14,16 +14,21 @@ public class BsuGame extends Game {
 		Rectangle rect = new Rectangle(0,0,480,320);
 		//定义第一个界面
 		HeadScreen hs_logo1 = new HeadScreen(this,"data/BsuLogo480-320.png","data/snd/chicken.wav",rect);
-		//第一个界面点击任意建时切换到第二个界面
-		hs_logo1.addObserver(new HeadScreen(this,"data/intro.png","data/snd/highjump.wav",rect){
+		HeadScreen hs_logo2 = new HeadScreen(this,"data/intro.png","data/snd/highjump.wav",rect){
 			@Override
 			public void update(Observable o, Object arg) {
 				BsuGame.this.setScreen(this);
 			}
-		});
-		 
+		};
+		HeadScreen hs_logo3 = new HeadScreen(this,"data/BsuLogo480-320.png","data/snd/chicken.wav",rect){
+			@Override
+			public void update(Observable o, Object arg) {
+				BsuGame.this.setScreen(this);
+			}
+		};
+
 		setScreen(hs_logo1);
+		hs_logo1.addObserver(hs_logo2);
+		hs_logo2.addObserver(hs_logo3);
 	}
-
-
 }
