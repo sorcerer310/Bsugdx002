80094ef948444a76a4dd716fc58bd79354cfb958
张永臣 <zhangyongchen333@163.com>
2013-05-07 13:27:17 +0800

    not use packfile

--- Gdx002/src/com/bsu/obj/GameMap.java
+++ Gdx002/src/com/bsu/obj/GameMap.java
@@ -1,12 +1,16 @@
 package com.bsu.obj;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.tiled.TileAtlas;
 import com.badlogic.gdx.graphics.g2d.tiled.TileMapRenderer;
 import com.badlogic.gdx.graphics.g2d.tiled.TiledLoader;
 import com.badlogic.gdx.graphics.g2d.tiled.TiledMap;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.bsu.tools.Configure;
+import com.bsu.tools.CustomerTiledAlisa;
 
 
 public class GameMap {
@@ -14,7 +18,8 @@ public class GameMap {
 	TileAtlas atlas;
 	public TileMapRenderer map_render;
 	public OrthographicCamera cam;
-
+	Vector3 camDirection = new Vector3(1, 1, 0);   
+    Vector2 maxCamPosition = new Vector2(0, 0);  
 	public GameMap(int level) {
 		// TODO Auto-generated constructor stub
 		get_map(level);
@@ -23,8 +28,11 @@ public class GameMap {
 	}
 	
 	public void get_map(int level){
-		map = TiledLoader.createMap(Gdx.files.internal("data/map/"+Configure.game_map_path_string[level]+".tmx"));
-		atlas = new TileAtlas(map, Gdx.files.internal("data/map/"));
+		FileHandle mapHandle = Gdx.files.internal("data/map/"+Configure.game_map_path_string[level]+".tmx");
+		map = TiledLoader.createMap(mapHandle);
+		FileHandle packages=Gdx.files.internal("data/map");   
+        atlas = new CustomerTiledAlisa(map, packages); 
 		map_render = new TileMapRenderer(map, atlas, 10, 10);
+		maxCamPosition.set(map_render.getMapWidthUnits(), map_render .getMapHeightUnits());
 	}
 }
---
+++ Gdx002/src/com/bsu/tools/CustomerTiledAlisa.java
@@ -0,0 +1,73 @@
+package com.bsu.tools;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.graphics.g2d.tiled.TileAtlas;
+import com.badlogic.gdx.graphics.g2d.tiled.TileSet;
+import com.badlogic.gdx.graphics.g2d.tiled.TiledMap;
+
+public class CustomerTiledAlisa extends TileAtlas {
+
+	public CustomerTiledAlisa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerTiledAlisa(TiledMap map, FileHandle inputDir) {
+		super(map, inputDir);
+		// TODO Auto-generated constructor stub
+		for (TileSet set : map.tileSets) {
+			if (set.imageName == null) {
+				continue;
+			}
+			FileHandle imageFile = getRelativeFileHandle(inputDir,
+					(set.imageName));
+			List<AtlasRegion> atlasRegions = new ArrayList<AtlasRegion>();
+			Texture texture = new Texture(imageFile);
+			for (int j = 0; j < texture.getHeight() / set.tileHeight; j++) {
+				int wn = texture.getWidth() / set.tileWidth;
+				for (int i = 0; i < wn; i++) {
+					AtlasRegion atlasRegion = new AtlasRegion(texture, i
+							* set.tileWidth, j * set.tileHeight, set.tileWidth,
+							set.tileHeight);
+					atlasRegion.index = j * wn + i;
+					atlasRegions.add(atlasRegion);
+				}
+			}
+			for (AtlasRegion reg : atlasRegions) {
+				regionsMap.put(reg.index + set.firstgid, reg);
+				if (!textures.contains(reg.getTexture())) {
+					textures.add(reg.getTexture());
+				}
+			}
+
+			System.out.println("textuare is ->" + textures);
+		}
+	}
+
+	private static FileHandle getRelativeFileHandle(FileHandle path,
+			String relativePath) {
+		if (relativePath.trim().length() == 0) {
+			return path;
+		}
+
+		FileHandle child = path;
+
+		StringTokenizer tokenizer = new StringTokenizer(relativePath, "\\/");
+		while (tokenizer.hasMoreTokens()) {
+			String token = tokenizer.nextToken();
+			if (token.equals("..")) {
+				child = child.parent();
+			} else {
+				child = child.child(token);
+			}
+		}
+
+		return child;
+	}
+
+}
