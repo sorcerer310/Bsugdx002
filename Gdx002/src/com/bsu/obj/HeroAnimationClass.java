76e63265aa53d562ca3eca828f44e4786f187357
张永臣 <zhangyongchen333@163.com>
2013-05-10 21:31:47 +0800

    。。。

--- Gdx002/src/com/bsu/obj/HeroAnimationClass.java
+++ Gdx002/src/com/bsu/obj/HeroAnimationClass.java
@@ -24,7 +24,6 @@ public class HeroAnimationClass {
 					region2.flip(true, false);
 				}
 			}
-			
 		}
 	}
 	/**
--- Gdx002/src/com/bsu/obj/Role.java
+++ Gdx002/src/com/bsu/obj/Role.java
@@ -14,42 +14,50 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.bsu.tools.Configure.STATE;
 
 public class Role extends Actor {
-	private String name = "";						//记录这个角色的名字
+	private String name = ""; // 记录这个角色的名字
 	private TextureRegion current_frame;
-	private float state_time;						//状态时间
-	private float effect_time;						//特效时间
-	public STATE state;								//英雄的当前状态
-	public static enum Type {HERO,ENEMY};			//英雄还是NPC
-	private Type type = null;						//ָ指定当前角色是英雄还是 NPC
+	private float state_time; // 状态时间
+	private float effect_time; // 特效时间
+	public STATE state; // 英雄的当前状态
 
-	private Animation ani_current;					//当前动画
-	private Animation ani_idle;						//站立动画 
-	private Animation ani_move;						//移动动画
-	private Animation ani_attack_n;					//基本攻击 
-	private Animation ani_attack_v;					//横向攻击
-	private Animation ani_attack_h;					//纵向攻击
+	public static enum Type {
+		HERO, ENEMY
+	}; // 英雄还是NPC
+
+	private Type type = null; // ָ指定当前角色是英雄还是 NPC
+
+	private Animation ani_current; // 当前动画
+	private Animation ani_idle; // 站立动画
+	private Animation ani_move; // 移动动画
+	private Animation ani_attack_n; // 基本攻击
+	private Animation ani_attack_v; // 横向攻击
+	private Animation ani_attack_h; // 纵向攻击
 	private boolean loop_flag;
 
-	int maxHp = 100; 								//总血量
-	int currentHp = 30; 							//当前血量
-	int attack_value;								//自身攻击力
-	int attacked_nums;								//目前被连击数量ֵ
-	int attack_type;								//当前攻击类型
+	int maxHp = 100; // 总血量
+	int currentHp = 30; // 当前血量
+	int attack_value; // 自身攻击力
+	int attacked_nums; // 目前被连击数量ֵ
+	int attack_type; // 当前攻击类型
 	private TextureRegion effect_current_frame;
 	private Animation ani_effect;
 
-	int margin = 2; 								//血条和人物之间的间隔
-	int pixHeight = 5; 								//血条高度
+	int margin = 2; // 血条和人物之间的间隔
+	int pixHeight = 5; // 血条高度
 	int titleWidth = 32;
 	TextureRegion pix;
 
-	private boolean isSelected;						//被选中等待操作？
+	private boolean isSelected; // 被选中等待操作？
+
 	/**
 	 * 角色初始化
-	 * @param t		表示当前角色的类型
-	 * @param n		该角色的名字
+	 * 
+	 * @param t
+	 *            表示当前角色的类型
+	 * @param n
+	 *            该角色的名字
 	 */
-	public Role(Type t,String n) {
+	public Role(Type t, String n) {
 		// TODO Auto-generated constructor stub
 		name = n;
 		type = t;
@@ -60,40 +68,43 @@ public class Role extends Actor {
 	}
 
 	/**
-	 * 根据类型获得资源 
+	 * 根据类型获得资源
+	 * 
 	 * @param type
 	 */
-	private void set_actor_base(Type type ) {
+	private void set_actor_base(Type type) {
 		this.type = type;
 		int actor_type = type == Type.HERO ? 2 : 5;
 		ani_idle = HeroAnimationClass.getAnimationIdle(actor_type);
 		ani_move = HeroAnimationClass.getAnimationMove(actor_type);
-		ani_attack_n = HeroAnimationClass
-				.getAnimationAttackN(actor_type);
-		ani_attack_v = HeroAnimationClass
-				.getAnimationAttackV(actor_type);
-		ani_attack_h = HeroAnimationClass
-				.getAnimationAttackH(actor_type);
+		ani_attack_n = HeroAnimationClass.getAnimationAttackN(actor_type);
+		ani_attack_v = HeroAnimationClass.getAnimationAttackV(actor_type);
+		ani_attack_h = HeroAnimationClass.getAnimationAttackH(actor_type);
 		set_ani_from_state(STATE.idle);
 	}
 
-	private void show_life_display() {
+	/**
+	 * 设置人物生命血条
+	 */
+	private void draw_life_display() {
 		pix = null;
 		Pixmap pixmap;
-		pixmap = new Pixmap(64, 8, Format.RGBA8888); 		//生成一张64*8的图片
-		pixmap.setColor(Color.BLACK); 						//设置颜色为黑色。
+		pixmap = new Pixmap(64, 8, Format.RGBA8888); // 生成一张64*8的图片
+		pixmap.setColor(Color.BLACK); // 设置颜色为黑色。
 		pixmap.drawRectangle(0, 0, titleWidth, pixHeight);
-		pixmap.setColor(Color.RED); 						//设置为黑色
+		pixmap.setColor(Color.RED); // 设置为黑色
 		pixmap.fillRectangle(0, 1, titleWidth * currentHp / maxHp,
-				pixHeight - 2); 
-		Texture pixmaptex = new Texture(pixmap); 
-		pix = new TextureRegion(pixmaptex, titleWidth, pixHeight); 
+				pixHeight - 2);
+		Texture pixmaptex = new Texture(pixmap);
+		pix = new TextureRegion(pixmaptex, titleWidth, pixHeight);
 		pixmap.dispose();
 	}
 
 	/**
 	 * 取得角色初始状态属性
-	 * @param type	角色类型
+	 * 
+	 * @param type
+	 *            角色类型
 	 */
 	private void get_values(Type type) {
 		currentHp = 100;
@@ -108,24 +119,29 @@ public class Role extends Actor {
 		state_time += Gdx.graphics.getDeltaTime();
 		effect_time += Gdx.graphics.getDeltaTime();
 		this.setSize(32, 32);
-		this.check_frame_finish();
-		show_life_display();
+		draw_life_display();
+		current_frame = ani_current.getKeyFrame(state_time, loop_flag);
 		if (current_frame != null) {
 			batch.draw(current_frame, getX(), getY());
 		}
-		if (effect_current_frame != null) {
-			batch.draw(effect_current_frame, getX(), getY());
+		if (ani_effect != null) {
+			effect_current_frame = ani_effect.getKeyFrame(effect_time, false);
+			if (effect_current_frame != null) {
+				batch.draw(effect_current_frame, getX(), getY());
+			}
 		}
 		if (isSelected)
-			batch.draw(pix, this.getX(), this.getY() + this.margin + 32); // ����
+			batch.draw(pix, this.getX(), this.getY() + this.margin + 32); // 画血条
 	}
 
 	/**
 	 * 角色攻击，目前npc只有普通攻击
-	 * @param enemy	攻击动作的角色
+	 * 
+	 * @param enemy
+	 *            攻击动作的角色
 	 */
 	public void hero_attack_other(Role enemy) {
-		if(enemy==null){
+		if (enemy == null) {
 			return;
 		}
 		if (type == Type.HERO) {
@@ -136,7 +152,12 @@ public class Role extends Actor {
 		}
 	}
 
-	// 被攻击类型0-3
+	/**
+	 * 被攻击类型0-3
+	 * 
+	 * @param attack_type
+	 * @param damage_value
+	 */
 	public void hero_isAttacked(int attack_type, int damage_value) {
 		get_effect_state(attack_type);
 		currentHp = currentHp - damage_value >= 0 ? currentHp - damage_value
@@ -155,13 +176,22 @@ public class Role extends Actor {
 		return isSelected;
 	}
 
-	// **********effect ani
+	/**
+	 * 取得被攻击时特效
+	 * 
+	 * @param index
+	 */
 	private void get_effect_state(int index) {
-		ani_effect = HeroEffectClass.get_effect(ani_effect,index);
+		ani_effect = HeroEffectClass.get_effect(ani_effect, index);
 		effect_time = 0;
 	}
 
-	// 根据敌人被攻击标记设置攻击类型 0-->普通 1-->1 2-->2 ;
+	/**
+	 * 根据敌人被攻击标记设置攻击类型 0-->普通 1-->纵向攻击 2-->横向攻击 ;
+	 * 
+	 * @param enemy_attack_num
+	 *            ，标记的被攻击次数
+	 */
 	private void set_attack_type(int enemy_attack_num) {
 		switch (enemy_attack_num) {
 		case 0:
@@ -177,24 +207,26 @@ public class Role extends Actor {
 			state = STATE.attack_h;
 			break;
 		}
-		attack_type=enemy_attack_num>3?3:enemy_attack_num;
+		attack_type = enemy_attack_num > 3 ? 3 : enemy_attack_num;
 		set_ani_from_state(state);
 	}
 
 	/**
 	 * 根据角色状态取得角色动画
-	 * @param s	状态值
+	 * 
+	 * @param s
+	 *            状态值
 	 */
-	public void set_ani_from_state(STATE s){
+	public void set_ani_from_state(STATE s) {
 		loop_flag = false;
-		state=s;
+		state = s;
 		if (state == STATE.idle) {
 			ani_current = ani_idle;
-			loop_flag=true;
+			loop_flag = true;
 		}
 		if (state == STATE.move) {
 			ani_current = ani_move;
-			loop_flag=true;
+			loop_flag = true;
 		}
 		if (state == STATE.attack_normal) {
 			ani_current = ani_attack_n;
@@ -207,28 +239,12 @@ public class Role extends Actor {
 		}
 		state_time = 0;
 	}
+
 	/**
 	 * 返回角色状态
 	 */
-	public STATE get_ani_from_state(){
+	public STATE get_ani_from_state() {
 		return state;
-	}
-	
-	private void check_frame_finish() {
-		current_frame = ani_current.getKeyFrame(state_time, loop_flag);
-		if (ani_current.isAnimationFinished(state_time)) {
-			set_ani_from_state(STATE.idle);
-			set_selected(false);
-		}
-		if (ani_effect != null) {
-			effect_current_frame = ani_effect.getKeyFrame(effect_time, false);
-			if (ani_effect.isAnimationFinished(effect_time)) {
-				effect_current_frame = null;
-				ani_effect = null;
-				set_selected(false);
-			}
-			
-		}
 	}
 
 	private void set_listener() {
@@ -239,6 +255,7 @@ public class Role extends Actor {
 				// TODO Auto-generated method stub
 				super.touchUp(event, x, y, pointer, button);
 			}
+
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y,
 					int pointer, int button) {
@@ -248,6 +265,7 @@ public class Role extends Actor {
 			}
 		});
 	}
+
 	public Type getType() {
 		return type;
 	}
