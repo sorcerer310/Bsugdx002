ebf50278c7c31f440e75e130fff4634d6279163d
fengchong <fengchong@126.com>
2013-05-19 18:41:52 +0800

    解决冲突

--- Gdx002/src/com/bsu/screen/CPanelMainScreen.java
+++ Gdx002/src/com/bsu/screen/CPanelMainScreen.java
@@ -12,8 +12,11 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.utils.Array;
 import com.bsu.head.CubocScreen;
 import com.bsu.make.ButtonFactory;
+import com.bsu.obj.Player;
+import com.bsu.obj.Role;
 import com.bsu.tools.Configure;
 import com.bsu.tools.GameTextureClass;
 
@@ -21,12 +24,14 @@ public class CPanelMainScreen extends CubocScreen implements Observer {
 	private Texture timg;
 	private Image background;
 	private Stage stage;
+
 	private Image ib_mb_equip;
 	private Image ib_mb_fight;
 	private Image ib_mb_role;
 	private Image ib_mb_shop;
 	private Image ib_mb_skill;
 	private Image ib_mb_update;
+	private Array<Image> heroImg = new Array<Image>();
 
 	public CPanelMainScreen(Game game) {
 		super(game);
@@ -167,6 +172,33 @@ public class CPanelMainScreen extends CubocScreen implements Observer {
 		stage.addActor(ib_mb_role);
 		stage.addActor(ib_mb_fight);
 		stage.addActor(ib_mb_shop);
+		Array<Role> playerRols = Player.getInstance().getPlayerFightRole();
+		for (int i = 0; i < playerRols.size; i++) {
+			Image roleImg = new Image(playerRols.get(i).roleTexture);
+			roleImg.setScale(0.5f);
+			stage.addActor(roleImg);
+			roleImg.setPosition(48, 246 - 55 * i);
+			heroImg.add(roleImg);
+			roleImg.addListener(new InputListener() {
+				@Override
+				public boolean touchDown(InputEvent event, float x, float y,
+						int pointer, int button) {
+					getSelectRole((Image) event.getListenerActor());
+					return super.touchDown(event, x, y, pointer, button);
+				}
+			});
+		}
+	}
+	/**
+	 * 通过点击Image取得相应的role
+	 * @param img
+	 */
+	private void getSelectRole(Image img) {
+		for(int i=0;i<heroImg.size;i++){
+			if(heroImg.get(i).equals(img)){
+				
+			}
+		}
 	}
 
 	@Override
--- Gdx002/src/com/bsu/tools/GameTextureClass.java
+++ Gdx002/src/com/bsu/tools/GameTextureClass.java
@@ -4,9 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.bsu.make.SkillFactory;
 
 /**
  * 加载游戏中所有Texture，其他资源加载之前加载
@@ -28,28 +25,19 @@ public class GameTextureClass {
 	public Texture logo66Rpg;
 
 	public Texture effect_texture;
-	public Texture role_texture, new_role_texture;
-	public Texture card_texture;
+	public Texture role_texture;
 	public Texture mPanel;
 	public Texture equipPanel;
 	public Texture fightPanel, rolePanel, selectRolePanel, shopPanel,
 			skillPanel, updatePanel;
 	public TextureAtlas textureatlas_mbutton;
-//	public Skin skin;					//暂时没用上，以后准备使用
-//	public Image mb_equip;				//主界面装备按钮
-//	public Image mb_fight;				//主界面战斗按钮
-//	public Image mb_role;				//主角色按钮 
-//	public Image mb_selectrole;			//选择角色按钮
-//	public Image mb_shop;				//商店按钮
-//	public Image mb_skill;				//技能按钮 
-//	public Image mb_update;				//升级按钮
-//	public Image mb_back;				//返回按钮
 	
+	public TextureRegion fc_photo, zyc_photo, h0_photo, h1_photo, h2_photo,
+			h3_photo, h4_photo, h5_photo;
+
 
 	private GameTextureClass() {
 		// TODO Auto-generated constructor stub
-		role_texture = new Texture(
-				Gdx.files.internal("data/game/hero/hero.png"));
 		effect_texture = new Texture(
 				Gdx.files.internal("data/game/hero/effect.png"));
 		mPanel = new Texture(Gdx.files.internal("data/menu/mpanel.png"));
@@ -62,18 +50,21 @@ public class GameTextureClass {
 		skillPanel = new Texture(Gdx.files.internal("data/menu/skillpanel.png"));
 		updatePanel = new Texture(
 				Gdx.files.internal("data/menu/updatepanel.png"));
-		new_role_texture = new Texture(
-				Gdx.files.internal("data/game/ui/Actor2.png"));
-		card_texture = new Texture(Gdx.files.internal("data/game/ui/Actor2.png"));
 		
 		
 		textureatlas_mbutton = new TextureAtlas(Gdx.files.internal("data/menu/mbutton.txt"));
-//		mb_equip = new Image(mb_atlas.findRegion("mb_equip"));
-//		mb_fight = new Image(mb_atlas.findRegion("mb_fight"));
-//		mb_role = new Image(mb_atlas.findRegion("mb_role"));
-//		mb_shop = new Image(mb_atlas.findRegion("mb_shop"));
-//		mb_skill = new Image(mb_atlas.findRegion("mb_skill"));
-//		mb_update = new Image(mb_atlas.findRegion("mb_update"));
-//		mb_back = new Image(mb_atlas.findRegion("back"));
+
+		role_texture = new Texture(
+				Gdx.files.internal("data/game/hero/Actor1.png"));
+		fc_photo=new TextureRegion(role_texture,0,0,96,96);
+		zyc_photo=new TextureRegion(role_texture,96,0,96,96);
+		h0_photo=new TextureRegion(role_texture,192,0,96,96);
+		h1_photo=new TextureRegion(role_texture,288,0,96,96);
+		h2_photo=new TextureRegion(role_texture,0,96,96,96);
+		h3_photo=new TextureRegion(role_texture,96,96,96,96);
+		h4_photo=new TextureRegion(role_texture,192,96,96,96);
+		h5_photo=new TextureRegion(role_texture,288,96,96,96);
+
 	}
+
 }
