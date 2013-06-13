2d4490a9ab5bd6fd59533b0f58782d8aa481e1e2
fengchong <fengchong@126.com>
2013-06-13 14:59:54 +0800

    丰富了道具内容

--- Gdx002/src/com/bsu/make/ItemFactory.java
+++ Gdx002/src/com/bsu/make/ItemFactory.java
@@ -29,7 +29,47 @@ public class ItemFactory {
 			item = new Item("史诗级技能精华",Item.Type.skillpart,GTC.getInstance().getSkillIcon(0),GTC.getInstance().role_head_frame.findRegion("frame_orange"));
 			break;
 		case 101:
-			item = new Item("战士卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_yellow"));
+			item = new Item("绿色战士卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_green"));
+			break;
+		case 102:
+			item = new Item("蓝色战士卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_blue"));
+			break;
+		case 103:
+			item = new Item("紫色战士卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_purple"));
+			break;
+		case 104:
+			item = new Item("橙色战士卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_orange"));
+			break;
+		case 105:
+			item = new Item("绿色射手卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_green"));
+			break;
+		case 106:
+			item = new Item("蓝色射手卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_blue"));
+			break;
+		case 107:
+			item = new Item("紫色射手卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_purple"));
+			break;
+		case 108:
+			item = new Item("橙色射手卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_orange"));
+			break;
+		case 109:
+			item = new Item("绿色元素法师卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_green"));
+			break;
+		case 110:
+			item = new Item("蓝色元素法师卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_blue"));
+			break;
+		case 111:
+			item = new Item("紫色元素法师卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_purple"));
+			break;
+		case 112:
+			item = new Item("橙色元素法师卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_orange"));
+			break;
+//		case 113:
+//			item = new Item("绿色战士卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_yellow"));
+//			break;
+//		case 101:
+//			item = new Item("绿色战士卡片",Item.Type.rolecard,GTC.getInstance().fight_texture,GTC.getInstance().role_head_frame.findRegion("frame_yellow"));
+			
 			break;
 		}
 		return item;
